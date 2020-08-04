a5975d1125543f5fb384dfe9d28d0fa62d0a4a0a
gzstatu <gzstatu@gmail.com>
2020-08-04 17:18:55 +0800

    'test'

---
+++ src/Lws/Wxhelper/Test.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Lws\Wxhelper;
+
+class Test
+{
+    public static function test()
+    {
+        echo "This is Wxhelper test";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
